fbf7fed9deba85e472abfc0bf7c0654e468b8443
sekhar1245 <sekhar1245@gmail.com>
2020-05-03 20:23:20 +0100

    understanding props.children tag

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Started react learning...!</h1>
         <Person   name="sekhar" age="31"/>
-        <Person  name="Manu" age="32"/>
+        <Person  name="Manu" age="32"> Play chess is my hobbie</Person>
         <Person name="Bharat" age="33"/>
 
       </div>
--- react_practice/src/Person/Person.js
+++ react_practice/src/Person/Person.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const person = (props)=>{
 
-    return <p> This is {props.name} and I am {props.age} years old</p>
+   return ( 
+   <div> 
+       <p> This is {props.name} and I am {props.age} years old</p> 
+
+       <h4 >{props.children}</h4>
+
+
+   </div>
+    )
+
+    
 };
 
 export default person;
