b7450022ddcffc939dcc0379e4e33c8db9950de6
sekhar1245 <sekhar1245@gmail.com>
2020-05-03 13:17:34 +0100

    integrated Person component

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Person from './Person/Person.js';
 
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Started react learning...!</h1>
+        <Person />
       </div>
     );
   }
